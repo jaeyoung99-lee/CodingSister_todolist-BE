fcecf00e0d103a380e6376e9b51abe59b11f2811
jy01.lee <jy01.lee@smarterbuild.net>
2024-10-17 23:02:17 +0900

    first commit

---
+++ controller/task.controller.js
@@ -0,0 +1,60 @@
+const Task = require("../model/Task");
+
+const taskController = {};
+
+taskController.createTask = async (req, res) => {
+  try {
+    const { task, isComplete } = req.body;
+    const newTask = new Task({ task, isComplete });
+    await newTask.save();
+    res.status(200).json({ status: "ok", data: newTask });
+  } catch (err) {
+    res.status(400).json({ status: "fail", error: err });
+  }
+};
+
+taskController.getTask = async (req, res) => {
+  try {
+    const taskList = await Task.find({}).select("-__v");
+    res.status(200).json({ status: "ok", data: taskList });
+  } catch (err) {
+    res.status(400).json({ status: "fail", error: err });
+  }
+};
+
+taskController.updateTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { isComplete } = req.body;
+    const updateTask = await Task.findByIdAndUpdate(
+      id,
+      { isComplete },
+      { new: true }
+    );
+    if (!updateTask) {
+      return res
+        .status(404)
+        .json({ status: "fail", message: "Task not found" });
+    }
+    res.status(200).json({ status: "ok", data: updateTask });
+  } catch (err) {
+    res.status(400).json({ status: "fail", error: err });
+  }
+};
+
+taskController.deleteTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTask = await Task.findByIdAndDelete(id);
+    if (deleteTask) {
+      return res
+        .status(404)
+        .json({ status: "ok", message: "Task deleted successfully" });
+    }
+    res.status(200).json({ status: "ok" });
+  } catch (err) {
+    res.status(400).json({ status: "fail", error: err });
+  }
+};
+
+module.exports = taskController;
